786e81b66a8d6ae68606ba0f618a5fcaffcb555a
Sven Haardiek <sven@haardiek.de>
2019-10-26 19:37:49 +0200

    Mock C Dependencies for Documentation
    Signed-off-by: Sven Haardiek <sven@haardiek.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-import s5a
 
 
 # -- Project information -----------------------------------------------------
@@ -52,3 +51,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ["gdal", "numpy"]

2417f39ad70342fdb93bd5964e76379d1b306bf1
Timo Brockmeyer <timo.brockmeyer@gmail.com>
2019-12-02 22:23:10 +0100

    version 0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(filename):
 
 setup(
     name='s5a',
-    version='0.2',
+    version='0.3',
     description='Sentinel-5 Algorithms',
     author='Emissions API Developers',
     license='MIT',
